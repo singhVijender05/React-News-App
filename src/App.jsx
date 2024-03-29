32899d4ac67f51617ad6263db2e71365a4a0646d
Vijender Singh <102641197+singhVijender05@users.noreply.github.com>
2024-03-29 09:48:01 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import {useNavigate} from 'react-router-dom'
-import Navbar from '../../newsapp/src/components/Navbar'
-import News from '../../newsapp/src/components/News'
+import Navbar from '../src/components/Navbar'
+import News from '../newsapp/src/components/News'
 import axios from 'axios'
 
 function App() {
